19478234a473ffe4999a24ba8d5bd6551fd3f8f9
luca-gif <luca.89m@gmail.com>
2022-09-23 16:32:20 +0200

    Stampo gli indirizzi

---
+++ Address.cs
@@ -0,0 +1,28 @@
+﻿public class Address
+{
+    string Name { get; set; }
+    string Surname { get; set; }
+    string Street { get; set; }
+    string City { get; set; }
+    string Province { get; set; }
+    string Zip { get; set; }
+
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }
+
+    public override string ToString()
+    {
+        string str = "";
+        str += $"Name: {Name} - Surname: {Surname} - Street: {Street} - City: {City} - Province: {Province} - Zip: {Zip}";
+        return str;
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+StreamReader fileAddress = File.OpenText("C:/Users/luca8/Dropbox/Il mio PC (LAPTOP-KO0VR89I)/Downloads/addresses.csv");
+
+fileAddress.ReadLine();
+
+while (!fileAddress.EndOfStream)
+{
+    string record = fileAddress.ReadLine();
+
+    string[] addressSplit = record.Split(',');
+
+    try
+    {
+       
+
+        string name = addressSplit[0].Trim();
+        string surname = addressSplit[1].Trim();
+        string street = addressSplit[2].Trim();
+        string city = addressSplit[3].Trim();
+        string province = addressSplit[4].Trim();
+        string zip = addressSplit[5].Trim();
+
+        Address address = new Address(name, surname, street, city, province, zip);
+        Console.WriteLine(address.ToString());
+    }
+    catch
+    {
+       
+    } 
+           
+}
+fileAddress.Close();
+
